0fef102c5a82de0eaf1d3399803a54ed1791705e
Simon Burmer <simon.burmer@sap.com>
2026-04-26 16:20:53 +0200

    test

--- web/next.config.ts
+++ web/next.config.ts
@@ -11,9 +11,6 @@ const nextConfig: NextConfig = {
       "../ESCO dataset - v1.2.1 - classification - en - csv/ISCO/EMP_TEMP_SEX_OCU_EDU_NB_A-filtered-2026-04-26.csv",
     ],
   },
-  turbopack: {
-    root: path.join(__dirname, ".."),
-  },
 };
 
 export default nextConfig;
